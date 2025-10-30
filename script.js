3f41595ff0a6f0f712c86f66d141b4c591dbcaf4
sadness4me <36943037+sadness4me@users.noreply.github.com>
2025-10-30 13:31:04 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Hiệu ứng scroll nhẹ nhàng
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
